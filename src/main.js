92da7839640b9f918aaf640a346145ed3c432731
mooooburg <drawyourmind@naver.com>
2019-08-29 14:47:30 +0900

    190829_1447 검색결과

--- src/main.js
+++ src/main.js
@@ -8,22 +8,32 @@
 // }).$mount('#app')
 
 
+import SearchModel from './models/SearchModel.js'
+
 new Vue({
   el: '#app',
   data: {
-    query: ''
+    query: '',
+    submitted: false,
+    searchResult: []
   },
   methods: {
     onSubmit(e){
-
+      this.search();
     },
     onKeyup(){
       if(!this.query.length) this.onReset();
     },
+    search(){
+      SearchModel.list().then(data =>{
+        this.submitted = true;
+        this.searchResult = data;
+      })
+    },
     onReset(){
       this.query = '';
       // todo 검색결과를 숨기는 ...
-      debugger;
+      // debugger;
     }
   }
 })
